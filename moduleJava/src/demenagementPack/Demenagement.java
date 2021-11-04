556d83d32d7aae6f8a54868ddb14d6a20b90f9a0
Virginie Villard <virginie.villard@le-campus-numerique.fr>
2021-11-04 16:56:30 +0100

    Kit 1 Exercice 6 fini, On a un input pour le nombre de cartons et pour la capacité du camion et il crée un "tableau" avec le résultat.

--- moduleJava/src/demenagementPack/Demenagement.java
+++ moduleJava/src/demenagementPack/Demenagement.java
@@ -1,5 +1,7 @@
 package demenagementPack ;
 
+import java.util.Scanner;
+
 public class Demenagement {
 	private String nom; // attributs
 	private int capCamion; 
@@ -13,8 +15,11 @@ public class Demenagement {
 	public void auTravail(int nbCartons){
 		int nbVoyage = 0;
 		int emport;
+
 		System.out.println("nbVoyage \t nbCartons \t emport \t capCamion");
+		
 		while (nbCartons > 0) {
+			
 			if(nbCartons >= capCamion) {
 				emport = capCamion;
 			}
@@ -23,12 +28,20 @@ public class Demenagement {
 			}
 			
 			nbVoyage++;
-			
+
 			System.out.println(nbVoyage + "\t \t " + nbCartons + "\t \t " + emport + "\t \t " + capCamion);
 			nbCartons = nbCartons - emport;
 		}
 		
-		System.out.println(nom + " possède un camion de capacité : " + capCamion + " m³");
+		//for(int rest = nbCartons; rest > 0; rest -= capCamion){
+        //    noVoyage++;
+        //    if (rest < capCamion){
+        //        emport = rest;
+        //    }
+        //    System.out.print(noVoyage + "\t\t\t\t" + rest + "\t\t\t" + emport + "\t\t" + capCamion + "\t\n");
+        //}
+		
+		System.out.println(nom + " possède un camion de capacité : " + capCamion + "cartons");
 		System.out.println("Début du déménagement");
 	}
 
--- moduleJava/src/demenagementPack/TestDem.java
+++ moduleJava/src/demenagementPack/TestDem.java
@@ -1,11 +1,21 @@
 package demenagementPack;
 
+import java.util.*;
+
 public class TestDem {
 	public static void main(String[] args) {
 		System.out.println("Bienvenue");
+		
+		Scanner clavier = new Scanner(System.in);
+		System.out.print("Saisissez le nombre de cartons que vous souhaitez que nos déménageurs déplacent : ");
+		int nbCartons = clavier.nextInt();
+		
+		System.out.print("Saisissez la capacité du camion choisi (nombre de cartons) : ");
+		int capCamion = clavier.nextInt();
+		
 		// on crée une "instance"
-		Demenagement dem = new Demenagement("Entreprise bretonne",9);
+		Demenagement dem = new Demenagement("Entreprise bretonne",capCamion);
 		// on appelle la méthode d’instance "auTravail()
-		dem.auTravail(34); 
+		dem.auTravail(nbCartons); 
 	}
 }
